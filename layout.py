000ffb645df1053fa5372c6162ffbb170d08a589
kiwon-swing <kiwon@theswing.co.kr>
2025-09-29 23:32:01 +0900

    텍스트 포멧팅

---
+++ layout.py
@@ -0,0 +1,81 @@
+import tkinter.font
+
+
+HSTEP, VSTEP = 13, 18
+WIDTH = 800
+
+
+class Text:
+    def __init__(self, text):
+        self.text = text
+
+
+class Tag:
+    def __init__(self, tag):
+        self.tag = tag
+
+
+class Layout:
+    def __init__(self, tokens):
+        self.display_list = []
+        self.line = []
+        self.cursor_x = HSTEP
+        self.cursor_y = VSTEP
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 12
+
+        for tok in tokens:
+            self.token(tok)
+        self.flush()
+
+    def flush(self):
+        if not self.line:
+            return
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+        baseline = self.cursor_y + 1.25 * max_ascent
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+        max_descent = max([metric["descent"] for metric in metrics])
+        self.cursor_y = baseline + 1.25 * max_descent
+        self.cursor_x = HSTEP
+        self.line = []
+
+    def word(self, word):
+        font = tkinter.font.Font(size=self.size, weight=self.weight, slant=self.style)
+        w = font.measure(word)
+        self.line.append((self.cursor_x, word, font))
+        if self.cursor_x + w > WIDTH - HSTEP:
+            self.flush()
+
+        self.cursor_x += w + font.measure(" ")
+
+    def token(self, tok):
+        if isinstance(tok, Text):
+            for word in tok.text.split():
+                self.word(word)
+        elif tok.tag == "i":
+            self.style = "italic"
+        elif tok.tag == "/i":
+            self.style = "roman"
+        elif tok.tag == "b":
+            self.weight = "bold"
+        elif tok.tag == "/b":
+            self.weight = "normal"
+        elif tok.tag == "small":
+            self.size -= 2
+        elif tok.tag == "/small":
+            self.size += 2
+        elif tok.tag == "big":
+            self.size += 4
+        elif tok.tag == "/big":
+            self.size -= 4
+        elif tok.tag == "br" or tok.tag == "br /":
+            self.flush()
+        elif tok.tag == "p":
+            self.flush()
+        elif tok.tag == "/p":
+            self.flush()
+            self.cursor_y += VSTEP

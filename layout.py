d9be46e1bb3941e74ade5d37f232471f08ad837e
kiwon-swing <kiwon@theswing.co.kr>
2025-09-30 19:35:38 +0900

    텍스트 캐싱

--- layout.py
+++ layout.py
@@ -15,6 +15,18 @@ class Tag:
         self.tag = tag
 
 
+FONTS = {}
+
+
+def get_font(size, weight, style):
+    key = (size, weight, style)
+    if key not in FONTS:
+        font = tkinter.font.Font(size=size, weight=weight, slant=style)
+        label = tkinter.Label(font=font)
+        FONTS[key] = (font, label)
+    return FONTS[key][0]
+
+
 class Layout:
     def __init__(self, tokens):
         self.display_list = []
@@ -28,29 +40,6 @@ class Layout:
         for tok in tokens:
             self.token(tok)
         self.flush()
-
-    def flush(self):
-        if not self.line:
-            return
-        metrics = [font.metrics() for x, word, font in self.line]
-        max_ascent = max([metric["ascent"] for metric in metrics])
-        baseline = self.cursor_y + 1.25 * max_ascent
-        for x, word, font in self.line:
-            y = baseline - font.metrics("ascent")
-            self.display_list.append((x, y, word, font))
-        max_descent = max([metric["descent"] for metric in metrics])
-        self.cursor_y = baseline + 1.25 * max_descent
-        self.cursor_x = HSTEP
-        self.line = []
-
-    def word(self, word):
-        font = tkinter.font.Font(size=self.size, weight=self.weight, slant=self.style)
-        w = font.measure(word)
-        self.line.append((self.cursor_x, word, font))
-        if self.cursor_x + w > WIDTH - HSTEP:
-            self.flush()
-
-        self.cursor_x += w + font.measure(" ")
 
     def token(self, tok):
         if isinstance(tok, Text):
@@ -79,3 +68,26 @@ class Layout:
         elif tok.tag == "/p":
             self.flush()
             self.cursor_y += VSTEP
+
+    def flush(self):
+        if not self.line:
+            return
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+        baseline = self.cursor_y + 1.25 * max_ascent
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+        max_descent = max([metric["descent"] for metric in metrics])
+        self.cursor_y = baseline + 1.25 * max_descent
+        self.cursor_x = HSTEP
+        self.line = []
+
+    def word(self, word):
+        font = get_font(self.size, self.weight, self.style)
+        w = font.measure(word)
+        self.line.append((self.cursor_x, word, font))
+        if self.cursor_x + w > WIDTH - HSTEP:
+            self.flush()
+
+        self.cursor_x += w + font.measure(" ")
